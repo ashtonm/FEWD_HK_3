179b7f7df8361b43271de357842070545b98ce8f
Michael Ashton <mickash@hotmail.com>
2013-11-18 19:20:58 +0800

    homework

---
+++ homework/week4/Divided Times/divided-times-obfuscated/js/main.js
@@ -0,0 +1,38 @@
+//$( "slide-down" ).hide();
+
+//$( "#internaional-drop" ).click(function() {
+ // $( "#internaional-drop" ).show( "fast" );
+//});
+
+//$( "#internaional-drop" ).click(function() {
+//$( "#slide-down" ).toggleClass( "#user-nav" );
+//});
+
+//$( "#internaional-drop" ).click(function() {
+//$( "slide-down").show( "fast" );
+//});
+
+$( ".international" ).click(function() {
+  $( "#slide-down" ).show( "fast" );
+});
+
+$( ".international" ).click(function() {
+  $( "#politics-drop").hide( "fast"); 
+ });
+
+$( ".international" ).click(function() {
+  $( "#business-drop-drop").hide( "fast"); 
+ });
+
+$( ".international" ).click(function() {
+  $( "#technology-drop-drop").hide( "fast"); 
+ });
+
+$( ".international" ).click(function() {
+  $( "#culture-drop").hide( "fast"); 
+ });
+
+$( ".international" ).click(function() {
+  $( "#blogs-drop").hide( "fast"); 
+ });
+//"#business-drop", "#technology-drop", "#culture-drop", "#blogs-drop" //
