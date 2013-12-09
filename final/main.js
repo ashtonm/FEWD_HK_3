d9dcded8e8fd6a9cfcded28b1bfc6f78f3e9cd92
Michael Ashton <mickash@hotmail.com>
2013-12-09 20:19:09 +0800

    update

---
+++ final/main.js
@@ -0,0 +1,50 @@
+// Comments wuth '//!'' show the pseudo code//
+
+//RSS feed display on page // 
+// 1. RSS feed contents (article header, description, rss feed owner/link) are displayed in html tags
+// 2. RSS feeds sorted randomly // 
+
+
+// RSS feed time taken insertion // 
+// 1. RSS feed scans <body> of each article & counts words
+// 2. Reading time is then calculated based on number of words in <body>
+// 3. Reading time (in minutes) is then inserted in class=timetaken
+
+
+//When button is clicked
+
+//Button click
+//1/ Button for class="time" and/or "feed" is clicked
+//2/ RSS feeds are sorted based on combination of buttons clicked on (more than 1 button can be clicked on)
+//3/ RSS feeds are sorted based on button click
+//*4/ To reset buttons are unchecked
+
+//- RSS feed click on 
+//1/ "article header" or "rss feed owner" is clicked on 
+//2/ Link is opened up in new page
+
+
+//RSS feeds // 
+
+//NEWS
+//http://qz.com/feed - Quartz
+//http://rss.cnn.com/rss/edition.rss - CNN
+//http://www.abc.net.au/services/rss/  - ABC just in
+//http://feeds.reuters.com/reuters/topNews
+
+
+//sport
+//http://feeds.reuters.com/reuters/sportsNews - reuters sport
+//http://www.espncricinfo.com/rss/content/story/feeds/0.xml
+//http://feeds.theage.com.au/rssheadlines/sport.xml
+
+
+//interest
+//http://news.nationalgeographic.com/index.rss
+//http://feeds.mashable.com/Mashable?format=xml
+//http://www.fastcompany.com/rss.xml
+//http://www.nationalgeographic.com/adventure/nga.xml
+
+
+
+
